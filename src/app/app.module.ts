616ff8cc313a54b9bd41f0ba3f18afcb22642a14
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-03-22 16:05:23 -0700

    following book for angular 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { NewUserComponent } from './new-user/new-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    NewUserComponent
   ],
   imports: [
     BrowserModule,
